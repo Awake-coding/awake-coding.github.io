4c6b6c2ec829ce3fd18a7d342262595ae5eaa260
Awake <kirich007rep@gmail.com>
2020-03-27 01:11:08 +0200

    brunch_site 2.0
    Fixed:
    - In HTML - comments;
    - In SASS - comments;
    - Were centered more than 3 blocks, which were displayed incorrectly in IE 11 (in sections: header, section--second, section--fourth, section--eighth, section--eleventh, footer);
    - Image adaptability in section--second, in IE 11 displayed was incorrectly.
    
    Added:
    - In HTML - description, keywords;
    - Animation when you click the mobile menu icon;
    - Hover animation for the menu and logo in the header;
    - 3D animated image when hovering or clicking in section--second;
    - Animation for circle when you hover or click on a block, circle increases in section--third;
    - Animation when you hover or click on a block with a picture + text, the picture and text are increased in section--fourth;
    - Animation when you hover or click on a block with three circles in section--fifth;
    - Animation when you hover or click on a block, the picture is enlarged in section--sixth;
    - Carousel and made adaptability for her.

--- brunch/js/common.js
+++ brunch/js/common.js
@@ -1,40 +1,29 @@
+
 /* main js file*/
 $(function() {
 
-	
 });
 
 /* MENU для телефонов */
 (function($){
-	/* Добавляет и убирает по клику на иконку меню 
-	класс  - menu__open*/
-	// $(function() {
-	// 	$('.menu__icon').on('click', function() {
-	// 		$(this).closest('.menu').toggleClass('menu__open');
-	// 	});
-	// });
-
-	/* Добавляет и убирает по клику на иконку меню 
-		класс menu__open */
+	/* Добавляет и убирает по клику на иконку меню, класс menu__open */
 	$('.menu__icon').click(function () {
 		/* проверка наличие указанного имени класса "menu__open" у элемента $('.menu')*/
 		if(!$('.menu').hasClass('menu__open'))
 			$("html,body").css("overflow","hidden");
 		else
-			$("html,body").css("overflow","visible ");
+			$("html,body").css("overflow","visible");
 		$(this).closest('.menu').toggleClass('menu__open');// открыть меню
 		// $(this).closest('.menu').toggleClass('menu__close');// закрыть меню
 
-		/* Убирает по клику на ссылку в меню 
-	 класс  - menu__open*/
+		/* Убирает по клику на ссылку в меню класс  - menu__open*/
 		$('.menu__links-item').click(function(){
 			if($('.menu').hasClass('menu__open'))
-				$("html,body").css("overflow","visible ");
+				$("html,body").css("overflow","visible");
 			$(".menu").removeClass('menu__open');
 		});
 	}); 
 })(jQuery);
-
 
 
 /* ПЛАВНЫЙ ПЕРЕХОД ПО НАЖАНИЮ ССЫЛКИ а С КЛАССОМ scrollto */
@@ -45,4 +34,33 @@ $(document).ready(function() {
 				jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 800);
 				return false;
 		});
+});
+
+
+function printSlideIndex() {
+	// document.querySelector('.js-index').innerHTML = this.currentSlide+1
+}
+var mySiema10 = new Siema({
+	selector: '.siema__carousel', //класс к какому применим слайдер
+	duration: 500, //Продолжительность слайд-перехода в миллисекундах
+	// perPage: 3, //Количество отображаемых слайдов
+	perPage: { //адаптивный вариант
+		0: 1, // для области просмотра c 0 до 800рх
+		800: 2, // 2 для области просмотра c 800рх до 1183px
+		1183: 3 // 3 для области просмотра c 1183px до Npx
+	},
+	startIndex: 0,
+	draggable: true,
+	multipleDrag: true,
+	threshold: 40,
+	loop: true, 
+});
+// setInterval(() => mySiema10.next(), 2000) // авто переключение через N количество милисекунд
+var prev2 = document.querySelector('.tenth__prev');
+var next2 = document.querySelector('.tenth__next');
+prev2.addEventListener('click', function() { 
+	mySiema10.prev();
+});
+next2.addEventListener('click', function() { 
+	mySiema10.next();
 });
