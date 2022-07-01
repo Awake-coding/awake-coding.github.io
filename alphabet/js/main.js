aafcc810532a191746fecb135dd519ad7a7938e7
Awake-coding <60107946+Awake-coding@users.noreply.github.com>
2022-07-01 09:00:26 +0300

    alphabet v2.0 - Updated
    RU:
       - Код стал гораздо комактнее и красивее.
       - JS
          - Выполнение начинается после полной загрузки HTML (когда построено DOM-дерево), весь код находиться в document.addEventListener('DOMContentLoaded'...){}
                - Было: window.onload = function Main() {}
          - Объект "ObjectAlphabet" теперь содержит масивы только больших букв
          - Убраны подсчеты длинны каждого массива, а то есть переменные (length__enBigArr, length__enSmallArr, length__ruBigArr, length__ruSmallArr)
          - Убраны проверки длинны массивов больших и маленьких букв (так как убраны массивы маленьких букв)
       - HTML
          - <main class="main">
          - section.table-section>h1.h1
          - <footer class="footer">
       - SASS
          - deleted "_media.sass"
          - created "_config.sass"
                - $font-default
                - media mixin
                - Color var
                - default CSS SETTINGS
          - main.sass
                - мелкие изменения и доработки
                - Буквам big: чётко прописано быть большими (text-transform: capitalize)
                - Буквам small: чётко прописано быть маленькими (text-transform: lowercase)
                - style footer
    
    EN:
       - The code has become much more compact and beautiful.
       - JS
          - Execution starts after the HTML is fully loaded (when the DOM tree is built), all code is in document.addEventListener('DOMContentLoaded'...){}
                - Was: window.onload = function Main() {}
          - "ObjectAlphabet" object now contains arrays of capital letters only
          - Removed calculations of the length of each array, i.e. variables (length__enBigArr, length__enSmallArr, length__ruBigArr, length__ruSmallArr)
          - Removed checks for the length of arrays of large and small letters (as arrays of small letters were removed)
       - HTML
          - <main class="main">
          - section.table-section>h1.h1
          - <footer class="footer">
       - SASS
          - deleted "_media.sass"
          - created "_config.sass"
                - $font-default
                - media mixin
                - Color var
                - default CSS SETTINGS
          - main.sass
                - minor changes and improvements
                - Letters big: are explicitly written to be large (text-transform: capitalize)
                - Small letters are explicitly written to be small (text-transform: lowercase)
                - style footer

--- alphabet/js/main.js
+++ alphabet/js/main.js
@@ -1,187 +1,160 @@
-'use strict';
 
-//___ Object ___
-/* Объект с масcивами алфавитов */
-const ObjectAlphabet = {
-  enBigArr: [
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
-  ],
-  enSmallArr: [
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-  ],
-  ruBigArr: [
-    'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я'
-  ],
-  ruSmallArr: [
-    'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'
-  ],
-  ruToEnBigArr: [
-    'A', 'B', 'V', 'G', 'D', 'E', 'Yo(E)', 'Zh', 'Z', 'I', 'Y(i)', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'Kh', 'Ts', 'Ch', 'Sh', 'Shch', '\'\'', 'Y', '\'', 'E', 'Yu(iu)', 'Ya(ia)'
-  ],
-  ruToEnSmallArr: []
-};
+document.addEventListener('DOMContentLoaded', function Main() {
+  'use strict';
 
-//___ Variables global ___
-let maxLength__Array = 0; // Переменная для подсчета максимальной длинны массива
-let idTable = 'table';    // id для create <table>
+  //: Object with arrays Alphabets :
+  const ObjectAlphabet = {
+    enArr: [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ],
+    ruArr: [ 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я' ],
+    ruToEnArr: [ 'A', 'B', 'V', 'G', 'D', 'E', 'Yo/E', 'Zh', 'Z', 'I', 'Y/I', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'Kh', 'Ts', 'Ch', 'Sh', 'Shch', '\'\'', 'Y', '\'', 'E', 'Yu/Iu', 'Ya/Ia' ]
+  };
 
-/* Самый длинный массив букв */
-for (let key in ObjectAlphabet) { // В key весь масcив одного из свойства обьекта
-  if ( maxLength__Array < ObjectAlphabet[key].length ) {
-    maxLength__Array = ObjectAlphabet[key].length;
+  //: Variables global :
+  let maxLength__Array  = 0;        // Переменная для подсчета максимальной длинны массива
+  let idTable           = 'table';  // id для create <table>
+
+  /*
+  ? Подсчет максимальной длинны надо переделать,
+  ? когда в массиве будет алфавит длиннее чем те что выведены в таблице,
+  ? то не создавать пустых ячеек для выведенных уже алфавитов */
+  /* Самый длинный массив букв */
+  for (let key in ObjectAlphabet) { // В key весь массив одного из свойства объекта
+    if (maxLength__Array < ObjectAlphabet[key].length) {
+      maxLength__Array = ObjectAlphabet[key].length;
+    }
   }
-}
-
-/* Длинна масcивов */
-const length__enBigArr   = ObjectAlphabet.enBigArr.length;   // (26)
-const length__enSmallArr = ObjectAlphabet.enSmallArr.length; // (26)
-const length__ruBigArr   = ObjectAlphabet.ruBigArr.length;   // (33)
-const length__ruSmallArr = ObjectAlphabet.ruSmallArr.length; // (33)
-//* ____________________________________________________________________
 
 
-
-//___ Main function ___
-window.onload = function Main() {
-  /* Create <table></table> */
-  if ( document.getElementById('inner__table') !== null ) {
-    document.getElementById('inner__table').innerHTML = `<table class="${idTable}" id="${idTable}"></table>`;
-  } else
-    /* ERROR 1 */ return errorMessageOutput('<center>Error (№1)</center> <br/>Не существует в index.html -- div c id="inner__table"');
-
-  /* Проверка длинны масcива больших букв с маленькими */
-  if ( length__ruBigArr == length__ruSmallArr &&
-       length__enBigArr == length__enSmallArr
-  ) {
-    /* Create Cols */
-    createCol('colNumber', '№', null, null);
-    createCol('colEn', 'En', ObjectAlphabet.enBigArr, ObjectAlphabet.enSmallArr);
-    createCol('colRu', 'Ru', ObjectAlphabet.ruBigArr, ObjectAlphabet.ruSmallArr);
-    createCol('colRuToEn', 'Ru > En', ObjectAlphabet.ruToEnBigArr, ObjectAlphabet.ruToEnSmallArr);
-    /* Error test */
-    // createCol('231', 'Ru > En1', ObjectAlphabet.ruToEnBigArr, null); // Error 3
-    // createCol('231', 'Ru > En1', null, ObjectAlphabet.ruToEnBigArr); // Error 3
-    // createCol(9, 6, ObjectAlphabet.ruToEnBigArr, ObjectAlphabet.ruToEnBigArr); // Error 4
+  /* В .html создан блок с id='table__wrap' ? */
+  if (document.getElementById('table__wrap') !== null) {
+    /* 🛠 create <table> (если создан) */
+    let table           = document.createElement('table');
+        table.id        = idTable;
+    document.getElementById('table__wrap').append(table);
   } else {
-    /* ERROR 2 */
-    const massage = `
-      <center>Error (№2)</center>
-      <br/><strong>For create table - size arrays differ.
-      <br/>Check please arrays: ("RU" !== "ru") or ("EN" !== "en").</strong>
-    `;
-    errorMessageOutput(massage);
-  }
-};
-//* ____________________________________________________________________
-
-
-
-//___Functions___👇
-/*
-  👇 Parameters - createCol()
-  ** idCol            - ('' - string, уникальное id - для столбца нового алфавита)
-  ** nameCol_HTML     - (text - для название столбца Алфавита)
-  ** arrayLetterBig   - (Массив больших букв или null, если вместо 2-х массивов задать null, то создастся столбик нумерации строк)
-  ** arrayLetterSmall - (Массив маленьких букв или null, если вместо 2-х массивов задать null, то создастся столбик нумерации строк)
-*/
-function createCol( idCol='', nameCol_HTML='', arrayLetterBig=[], arrayLetterSmall=[] )
-{
-  if ( arrayLetterBig == null && arrayLetterSmall !== null ||
-       arrayLetterBig !== null && arrayLetterSmall == null
-  ) {
-    /* ERROR 3 */
-    const massage = `
-      <center>Error (№3)</center>
-      <br/> В функцию createCol()
-      <br/> ---- можно передать только такие парметры:
-      <br/>( \'String\', \'String\', null, null ) -- создастся столбик нумерации строк
-      <br/> ---- или такие:
-      <br/>( \'String\', \'String\', [ 'A', 'B'... ], [ 'a', 'b'... ] ) -- создастся столбик с алфавитом
+    /* ERROR 1 (если не создан) */ const massage = `
+        <center>Error (№1)</center><br/>
+        В .html файле НЕ создан блок с id='table__wrap' или написан с ошибкой
     `;
     return errorMessageOutput(massage);
   }
 
-  if ( idCol.constructor === String && nameCol_HTML.constructor === String &&
-       arrayLetterBig === null && arrayLetterSmall === null ||
-       idCol.constructor === String && nameCol_HTML.constructor === String &&
-       arrayLetterBig.constructor === Array && arrayLetterSmall.constructor === Array
-  ) {
-    let col;                 // Столбец
-    let nameCol;             // Имя столбца
-    let cell;                // Ячейка
-    let letterBig;           // Большая буква с маcсива
-    let letterSmall;         // Маленькая буква с масcива
 
-    //// create column
-    col = document.createElement('div');
-    col.className = 'col';
-    col.id = idCol;
-    document.getElementById(idTable).append(col);
 
-    //// create name column
-    nameCol = document.createElement('tr');
-    nameCol.className = 'nameCol';
-    nameCol.innerHTML = '<th class="name" colspan=2>' + nameCol_HTML + '</th>';
-    document.getElementById(idCol).append(nameCol);
 
-    if ( arrayLetterBig === null && arrayLetterSmall === null ) {
-      //// create cell number
-      for (let i = 0; i < maxLength__Array; i++) {
-        cell = document.createElement('tr');
-        cell.className = 'number';
-        cell.innerHTML = '<td>' + (i + 1) + '</td>'; // Начало с 1
-        document.getElementById(idCol).append(cell);
+  //*___ main actions ___*/
+  /* creating columns with the given parameters */
+  createCol('colNumber',  '№',        null);
+  createCol('colEn',      'En',       ObjectAlphabet.enArr);
+  createCol('colRu',      'Ru',       ObjectAlphabet.ruArr);
+  createCol('colRuToEn',  'Ru > En',  ObjectAlphabet.ruToEnArr);
+
+
+
+
+
+
+
+
+
+  //: ___ Functions ___👇*/
+
+  //*: createCol :*/
+  /*
+    👇 Parameters - createCol()
+    ** idCol            - ('' - string, уникальное id - для столбца нового алфавита)
+    ** nameCol__th      - (text - для название столбца Алфавита)
+    ** arrayLetters     - (Массив букв или null, если вместо массива задать null, то создастся столбик нумерации строк)
+  */
+  function createCol(idCol='', nameCol__th='', arrayLetters=[])
+  {
+    /* Массив пустой? */
+    if (arrayLetters !== null && arrayLetters.length === 0) {
+      /* ERROR 2 */ const massage = `
+          <center>Error (№2)</center><br/>
+          В функцию createCol() вы передали пустой массив для столбика "${nameCol__th}". <br/><br/>
+          — Пожалуйста укажите правильное имя массива алфавита в объекте ObjectAlphabet <br/><br/>
+          или <br/><br/>
+          — Добавьте буквы алфавита к массиву с указанным именем
+      `;
+      errorMessageOutput(massage);
+    }
+
+    if (idCol.constructor         === String &&
+        nameCol__th.constructor   === String &&
+        arrayLetters              === null
+        ||
+        idCol.constructor         === String &&
+        nameCol__th.constructor   === String &&
+        arrayLetters.constructor  === Array)
+    {
+      /* 🛠 create column (Столбец div) 👈 */
+      let col__div            = document.createElement('div');
+          col__div.className  = 'col';
+          col__div.id         = idCol;
+      document.getElementById(idTable).append(col__div);
+
+      /* 🛠 create name column (Имя столбца) 👈 */
+      let colName__th           = document.createElement('tr');
+          colName__th.className = 'nameCol';
+          colName__th.innerHTML = '<th class="name" colspan=2>' + nameCol__th + '</th>';
+      document.getElementById(idCol).append(colName__th);
+
+      if (arrayLetters === null) {
+        /* 🛠 create cell number (Ячейка number) 👈 */
+        for (let i = 0; i < maxLength__Array; i++) {
+          let cell__td            = document.createElement('tr');
+              cell__td.className  = 'number';
+              cell__td.innerHTML  = '<td>' + (i + 1) + '</td>'; // Начало с 1
+          document.getElementById(idCol).append(cell__td);
+        }
+      } else {
+        /* 🛠 create cell letters (Ячейка letters)👈 */
+        for (let i = 0; i < maxLength__Array; i++) {
+          let letterBig   = arrayLetters[i];
+          let letterSmall = arrayLetters[i];
+
+          /* Массив алфавита содержит, на данной итерации, букву? */
+          if (letterBig == undefined && letterSmall == undefined) {
+                letterBig   = '<div class="empty__letter"></div>';
+                letterSmall = '<div class="empty__letter"></div>';
+          } else {
+                letterSmall = /* '( '+  */letterSmall/*  + ' )' */;
+          }
+
+          let cell__td            = document.createElement('tr');
+              cell__td.className  = 'letters';
+              cell__td.innerHTML  = '<td><div class="big">' + letterBig    + '</div>' +
+                                      '<div class="small">'   + letterSmall  + '</div></td>';
+          document.getElementById(idCol).append(cell__td);
+        }
       }
     } else {
-      //// create cell letters
-      for (let i = 0; i < maxLength__Array; i++) {
-        letterBig = arrayLetterBig[i];
-        letterSmall = arrayLetterSmall[i];
-
-        if ( letterBig == undefined && letterSmall == undefined ) {
-          letterBig = '<div class="empty__letter"></div>';
-          letterSmall = '<div class="empty__letter"></div>';
-        } else if ( letterBig == undefined || letterSmall == undefined ) {
-          if ( letterBig == undefined) {
-            letterBig = '<div class="empty__letter"></div>';
-          } else letterSmall = '<div class="empty__letter"></div>';
-        } else {
-          letterSmall = '( '+ letterSmall + ' )';
-        }
-
-        cell = document.createElement('tr');
-        cell.className = 'letters';
-        cell.innerHTML = '<td><div class="big">' + letterBig + '</div>' +
-                         '<div class="small">'   + letterSmall + '</div></td>';
-        document.getElementById(idCol).append(cell);
-      }
+      /* ERROR 3*/ const massage = `
+          <center>Error (№3)</center><br/>
+          В функцию createCol() можно передать только такие параметры: <br/><br/><ul><li>
+          ( \'String\', \'String\', null ) - создастся столбик нумерации строк;</li></ul><br/>
+          OR<br/><br/><ul><li>
+          ( \'String\', \'String\', ObjectAlphabet.&lt;nameArray&gt; ) - создастся столбик с алфавитом.</li></ul>
+      `;
+      return errorMessageOutput(massage);
     }
-  } else {
-    /* ERROR 4*/
-    const massage = `
-      <center>Error (№4)</center>
-      <br/> В функцию createCol()
-      <br/> ---- можно передать только такие парметры:
-      <br/>( \'String\', \'String\', null, null ) -- создастся столбик нумерации строк
-      <br/> ---- или такие:
-      <br/>( \'String\', \'String\', [ 'A', 'B'... ], [ 'a', 'b'... ] ) -- создастся столбик с алфавитом
-    `;
-    errorMessageOutput(massage);
   }
-}
 
-/* Error Message function */
-function errorMessageOutput(textError='') {
-  let arrAlertClassElements = document.getElementsByClassName('alert');
 
-    if (arrAlertClassElements[0] == undefined) {
-      let errorMessage; // Переменная для вывода ошибки вместо таблицы
 
-      errorMessage = document.createElement('div');
-      errorMessage.className = 'alert';
-      errorMessage.innerHTML = textError;
+  //*: errorMessageOutput :*/
+  function errorMessageOutput(textError = '') {
+    let arrAlertClassElements = document.getElementsByClassName('error-alert');
 
-      document.getElementById('alert__block').append(errorMessage);
-    }
-}
-//* ____________________________________________________________________
+      /* Отображать только 1 ошибку */
+      if (arrAlertClassElements[0] == undefined) {
+        let errorMessage;
+            errorMessage           = document.createElement('div');
+            errorMessage.className = 'error-alert';
+            errorMessage.innerHTML = textError; // Передаем текст ошибки с параметра
+        document.getElementById('table__error--wrap').append(errorMessage);
+      }
+  }
+
+});
